bc4474dc20d9a331e21c41c55a7c80eb1c46ae5f
Subhash Chandra <phoenix@SilverHawk.(none)>
2011-07-16 13:13:10 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Protsahan::Application.config.secret_token = '75f96b4b5f48c0d6bd5105ab174abb299ddfdf1b602cc8d63b9e9b3a7b7039f7f953c6b973bf433d0fa1e4732be44e22f430dbeaa4a0ef60c5199b77541bc2b4'
