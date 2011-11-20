148d76debe2ab35d184090b1299f747e8d5851b1
Subhash Chandra <TMaYaD+github@gmail.com>
2011-11-20 21:46:07 +0530

    created new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Protsahan::Application.config.secret_token = 'aec8909ecf59a37790f83ed2bb7d113672049fb2c9669e33830e6f70261c1b9c7681ace42494b7a0d5a2545d4b048ed5ddb51d488c43072fd9dbaabca090951f'
