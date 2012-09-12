d8bf10329410ac312941d3c6ff4019860771073b
Subhash Chandra <TMaYaD@LoonyB.in>
2012-09-13 00:32:14 +0530

    added semi static pages support

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,12 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.pages = function(req, res){
+  res.render('pages/' + req.params[0], { title: 'Express' }, function(err, html) {
+    if (err)
+      res.send(404, 'Sorry, we cannot find that!');
+    else
+      res.send(200, html);
+  });
+};
